fd8639e27a1f99f23ec5e5da93dedd17fc66f184
Demo Macro <abc@imst.xyz>
2020-05-14 23:01:33 +0800

    Git Init

---
+++ pages/blog/_blog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-html="blogPost.default"></div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { blogPost: payload };
+    else
+      return {
+        blogPost: await require(`~/assets/content/blog/${params.blog}.md`),
+      };
+  },
+};
+</script>
